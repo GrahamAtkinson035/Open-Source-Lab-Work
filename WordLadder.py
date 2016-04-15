2fbb9faad9e6257425999f8456aee570d5682e34
GrahamAtkinson035 <0.035ounces@gmail.com>
2016-04-15 12:46:33 -0400

    Update WordLadder.py

--- WordLadder.py
+++ WordLadder.py
@@ -4,6 +4,16 @@ import networkx as nx
 #-------------------------------------------------------------------
 #   The Words/Ladder graph of Section 1.1
 #-------------------------------------------------------------------
+def anagrams(word):
+    if len(word) < 2:
+        return word
+    else:
+        tmp = []
+        for i, letter in enumerate(word):
+            for j in anagrams(word[:i]+word[i+1:]):
+                tmp.append(j+letter)
+    return tmp
+
 def generate_graph(words):
     from string import ascii_lowercase as lowercase
     G = nx.Graph(name="words")
@@ -13,8 +23,11 @@ def generate_graph(words):
             left, c, right = word[0:i], word[i], word[i+1:]
             j = lookup[c] # lowercase.index(c)
             for cc in lowercase[j+1:]:
-                yield left + cc + right
-    candgen = ((word, cand) for word in sorted(words)
+                ana_word = left + cc + right
+                ana_list = anagrams(ana_word)
+                for i in ana_list:
+                    yield i
+    candgen = ((word, cand) for word in sorted(words) 
                for cand in edit_distance_one(word) if cand in words)
     G.add_nodes_from(words)
     for word, cand in candgen:
@@ -37,8 +50,8 @@ def words_graph():
 if __name__ == '__main__':
     from networkx import *
     G=words_graph()
-    print("Loaded words5.txt containing 5757 five-letter English words.")
-    print("Two words are connected if they differ in one letter.")
+    print("Loaded words_dat.txt containing 5757 five-letter English words.")
+    print("Two words are connected if they differ in one letter in any position.")
     print("Graph has %d nodes with %d edges"
           %(number_of_nodes(G),number_of_edges(G)))
     print("%d connected components" % number_connected_components(G))
