3e7f299f2ca72358245536c2c615be794b15c4c6
GrahamAtkinson035 <0.035ounces@gmail.com>
2016-04-14 21:00:17 -0400

    Update WordLadder4.py

--- WordLadder4.py
+++ WordLadder4.py
@@ -1,8 +1,19 @@
+
 import networkx as nx
 
 #-------------------------------------------------------------------
 #   The Words/Ladder graph of Section 1.1
 #-------------------------------------------------------------------
+def anagrams(word):
+    if len(word) < 2:
+        return word
+    else:
+        tmp = []
+        for i, letter in enumerate(word):
+            for j in anagrams(word[:i]+word[i+1:]):
+                tmp.append(j+letter)
+    return tmp
+
 def generate_graph(words):
     from string import ascii_lowercase as lowercase
     G = nx.Graph(name="words")
@@ -12,8 +23,11 @@ def generate_graph(words):
             left, c, right = word[0:i], word[i], word[i+1:]
             j = lookup[c] # lowercase.index(c)
             for cc in lowercase[j+1:]:
-                yield left + cc + right
-    candgen = ((word, cand) for word in sorted(words)
+                ana_word = left + cc + right
+                ana_list = anagrams(ana_word)
+                for i in ana_list:
+                    yield i
+    candgen = ((word, cand) for word in sorted(words) 
                for cand in edit_distance_one(word) if cand in words)
     G.add_nodes_from(words)
     for word, cand in candgen:
@@ -23,21 +37,21 @@ def generate_graph(words):
 def words_graph():
     """Return the words example graph from the Stanford GraphBase"""
     import gzip
-    fh=gzip.open('words4_dat.txt.gz','r')
+    fh=gzip.open('words4.dat.gz','r')
     words=set()
     for line in fh.readlines():
         line = line.decode()
         if line.startswith('*'):
             continue
-        w=str(line[0:5])
+        w=str(line[0:4])
         words.add(w)
     return generate_graph(words)
 
 if __name__ == '__main__':
     from networkx import *
     G=words_graph()
-    print("Loaded words4_dat.txt.gz containing all four-letter English words.")
-    print("Two words are connected if they differ in one letter.")
+    print("Loaded words_dat.txt containing A BUNCH OF 4-letter English words.")
+    print("Two words are connected if they differ in one letter in any position.")
     print("Graph has %d nodes with %d edges"
           %(number_of_nodes(G),number_of_edges(G)))
     print("%d connected components" % number_connected_components(G))
